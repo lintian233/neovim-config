e4145de392c9d18f7d739c98fb99861b5dbe1f62
XD <2540437977@qq.com>
2024-12-14 16:20:40 +0800

    lazy.nvim config

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 
 local set = vim.o
 
--- 行号
 set.number = true
 
 -- 相对行号
@@ -23,10 +22,51 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 -- keybindings
 local opt = { noremap = true, silent = true }
 
+-- Map
+vim.g.mapleader = " "
+
+-- Ctrl + hjkl => Ctrl + w hjkl
 vim.keymap.set("n", "<C-h>", "<C-w>h", opt)
 vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
 vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
 vim.keymap.set("n", "<C-l>", "<C-w>l", opt)
 
+-- Ctrl + v or s => Ctrl + w + v s
+vim.keymap.set("n", "<Leader>v", "<C-w>v", opt)
+vim.keymap.set("n", "<Leader>s", "<C-w>s", opt)
+
+-- 更好的行跳
+-- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
+vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
+
+-- lazy.nvim 的配置
+-- lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+	{
+		"RRethy/nvim-base16",
+		lazy = true,
+	},
+	{
+  		"folke/persistence.nvim",
+  		event = "BufReadPre", -- this will only start session saving when an actual file was opened
+	}
+})
+
+vim.cmd.colorscheme("base16-tender")
 
 
